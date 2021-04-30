1c2b516600cc22d484b8dc1e75b46c42bc1430e2
Mike Edwards <michael.edwards.4108@gmail.com>
2021-04-30 13:09:33 -0400

    Created variable for destination array

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 "use strict";
 
+let destination = ["Berlin, Germany", "Melbourne, Australia", "Geneva, Switzerland", "Oslo, Norway", "Quito, Ecuador", "Ankara, Turkey", "Seville, Spain", "Cairo, Egypt", "Moscow, Russia", "Tokyo, Japan"];
