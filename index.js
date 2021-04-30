ebc180e8595a4c6ad3d0cd13b41aee124fc2ae3f
Mike Edwards <michael.edwards.4108@gmail.com>
2021-04-30 15:52:34 -0400

    Changed the values of the restaurant & entertainment arrays. Moved/commented out dayTripLocation variable. Created intro alert. Created console.log for elements of the day trip (location, restaurant, transportation, & entertainment).

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 
 let destination = ["Berlin, Germany", "Melbourne, Australia", "Geneva, Switzerland", "Oslo, Norway", "Quito, Ecuador", "Ankara, Turkey", "Seville, Spain",
  "Cairo, Egypt", "Moscow, Russia", "Tokyo, Japan"];
-let restaurant = ["Fast Food", "Cheap Local Cuisine", "Expensive Local Cuisine", "Foreign Food"]
+let restaurant = ["Fast Food", "Inexpensive Local Cuisine", "Expensive Local Cuisine", "Other Foreign Food"]
 let transportation = ["Boat", "Plane", "Train", "Automobile"]
-let entertainment = ["Fair", "Go-Karts", "Paintball", "Dancing", "Laser Tag"]
+let entertainment = ["site-seeing", "going on a Brewery Tour", "seeing a Commedy Show", " going Dancing", "going on a Museum Trip"]
 function randGenerator(arr){
     let randGenerator = Math.floor((Math.random() * arr.length));
     return arr[randGenerator];
 }
-let dayTripLocation = parseInt(prompt("Hello! Select a number between 0 and 9 to see where you will randomly go for your day trip!"));
+alert("You are about to be randomly assigned a day trip somewhere in the world!")
+console.log("You will be going to " + randGenerator(destination) + "!");
+console.log("The type of restuarant you will eat at will be " + randGenerator(restaurant) + ".");
+console.log("Your mode of transportation will be by " + randGenerator(transportation) + ".");
+console.log("When you are there, you will be " + randGenerator(entertainment) + "!");
 
 
 
@@ -36,4 +40,4 @@ let dayTripLocation = parseInt(prompt("Hello! Select a number between 0 and 9 to
 // if(dayTripLocation !== destination[i]){
 //     console.log("Please select a number 0 - 9.")
 // }
-
+// let dayTripLocation = parseInt(prompt("Hello! Select a number between 0 and 9 to see where you will randomly go for your day trip!"));
